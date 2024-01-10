764a748173309d7324fd60ee6ea33ef9f338f53d
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2024-01-10 10:53:14 +0000

    creating NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import "tailwindcss/tailwind.css"; // Import Tailwind CSS styles
+import "./index.css";
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <>
-      <div>App</div>
+      <div className="app bg-black h-screen">
+        <Navbar />
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
+import { url } from "inspector";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#f8f4eb",
+        "gray-50": "#efe6e6",
+        "gray-100": "#FFE1E0",
+        "gray-500": "#5e0000",
+        "primary-100": "#ffffff",
+        "primary-500": "#ffffff",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-black": "linear-gradient(147deg, #4d4855 0%, #000000 74%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        monserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
-}
-
+};
